3c1a2465a66ed7ed1d892e781278ef26321f2677
MaxVortman <maka.abort@gmail.com>
2018-11-03 21:54:36 +0300

    DbContext added.

---
+++ SPbSU.VK.TimeTable/VkTimeTableContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SPbSU.VK.TimeTable
+{
+	public class VkTimeTableContext : DbContext
+	{
+		public VkTimeTableContext(DbContextOptions options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
